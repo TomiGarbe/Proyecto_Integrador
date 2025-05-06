34cdcaa603918987608b56b53fc498c7fa5df85a
TomiGarbe <2105905@ucc.edu.ar>
2025-05-06 18:29:11 -0300

    feat(sucursales): Actualizacion y cambios en frontend - Actualizacion de dependencias del frontend - Cambios en navbar
    Refs: #6

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,12 +11,7 @@ const AppNavbar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/users">Usuarios</Nav.Link>
             <Nav.Link as={Link} to="/sucursales">Sucursales</Nav.Link>
-            <Nav.Link as={Link} to="/cuadrillas">Cuadrillas</Nav.Link>
-            <Nav.Link as={Link} to="/preventivos">Preventivos</Nav.Link>
-            <Nav.Link as={Link} to="/mantenimientos-preventivos">Mantenimientos Preventivos</Nav.Link>
-            <Nav.Link as={Link} to="/mantenimientos-correctivos">Mantenimientos Correctivos</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
