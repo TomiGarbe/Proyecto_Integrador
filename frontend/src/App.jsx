a41c5bce4e9f302ac84326751bbf3b80752c4300
TomiGarbe <2105905@ucc.edu.ar>
2025-05-07 15:41:38 -0300

    feat(sucursales): Cambios de diseño del frontend - Creamos la pagina mantenimiento - Agregamos el logo de la empresa - Configuracion de rutas en app - Ajustes en las rutas en navbar, home y mantenimiento - Cambios de diseño y colores del frontend
    Refs: #6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Sucursales from './pages/Sucursales';
+import Mantenimiento from './pages/Mantenimiento';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <main className="flex-grow-1">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/mantenimiento" element={<Mantenimiento />} /> 
             <Route path="/sucursales" element={<Sucursales />} />
           </Routes>
         </main>
