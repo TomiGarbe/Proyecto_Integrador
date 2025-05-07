b336cea4784f47eb32129beb70491634102bdb0a
TomiGarbe <2105905@ucc.edu.ar>
2025-05-07 11:12:59 -0300

    feat(sucursales): Configuracion y tests unitarios del backend - Ajuste de configuracion de base de datos para tests unitarios - Ajuste de requerimientos para tests - Creamos archivos de configuracion para tests como pytest.ini y conftest.py - Agregamos tests unitarios de sucursales y zonas - Configuracion de reportes de resultados y covertura de tests unitarios
    Refs: #6

--- backend/src/config/database.py
+++ backend/src/config/database.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv(dotenv_path="./env.config")
-DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base.metadata.create_all(bind=engine)
